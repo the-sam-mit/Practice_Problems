cb77c36c54cd176a8a8c8a4cff11d6eb76ebcbc4
the-sam-mit <47500983+the-sam-mit@users.noreply.github.com>
2019-04-02 00:54:29 +0530

    Ad-Hoc(Alkhwarizm' 18)

---
+++ HELPVOLD(Alkhwarizm '18).cpp.c
@@ -0,0 +1,28 @@
+//Ad-Hoc(ALKHWARIZM' 18)
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ff first 
+#define ss second
+#define pb(x) push_back(x)
+#define mp(x,y) make_pair(x,y)
+#define in(x) insert(x)
+#define ffor(i,a,b) for(int i=a;i<b;i++)
+#define bfor(i,a,b) for(int i=a;i>=b;i--)
+#define PI 3.14159265358979323846
+#define SP(x) setprecision(x)
+#define M 1000000007
+#define reflex ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define set_bit(x) __builtin_popcount(x)
+
+int main(){
+    reflex;
+    int n;
+    cin>>n;
+    ll a[n];
+    ffor(i,0,n) cin>>a[i];
+    sort(a,a+n);
+    ll ans=0;
+    ffor(i,0,n-1) ans+=a[i]*a[i+1];
+    cout<<ans<<endl;
+}
