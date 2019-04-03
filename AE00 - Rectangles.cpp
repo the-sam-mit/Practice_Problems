4096cdbdcd70c91609c70206202d6a45cce4670b
the-sam-mit <47500983+the-sam-mit@users.noreply.github.com>
2019-04-04 00:36:32 +0530

    SPOJ - Basic Maths

---
+++ AE00 - Rectangles.cpp
@@ -0,0 +1,26 @@
+// SPOJ - Basic Maths
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ff first 
+#define ss second
+#define pb(x) push_back(x)
+#define mp(x,y) make_pair(x,y)
+#define in(x) insert(x)
+#define ffor(i,a,b) for(int i=a;i<b;i++)
+#define bfor(i,a,b) for(int i=a;i>=b;i--)
+#define PI 3.14159265358979323846
+#define SP(x) setprecision(x)
+#define reflex ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+int main(){
+    reflex;
+    int n;
+    cin>>n;
+    ll ans=0;
+    ffor(i,1,n+1){
+        ans+=(n/i);
+    }
+    ll x=sqrt(n);
+    cout<<((ans+x)/2)<<endl;
+}
