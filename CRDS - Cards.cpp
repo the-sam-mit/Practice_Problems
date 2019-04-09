a090e92907a5f137d2fb0013781a47f8b9342869
the-sam-mit <47500983+the-sam-mit@users.noreply.github.com>
2019-04-09 13:22:35 +0530

    SPOJ - Basic Maths

---
+++ CRDS - Cards.cpp
@@ -0,0 +1,34 @@
+//Basic Maths
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ull unsigned long long int
+#define ff first 
+#define ss second
+#define pb(x) push_back(x)
+#define mp(x,y) make_pair(x,y)
+#define in(x) insert(x)
+#define rep(i,a,b)  for (__typeof((b)) i=(a);i<=(b);i++)
+#define nrep(i,a,b)  for (__typeof((b)) i=(b);i>=(a);i--)
+#define ffor(i,a,b) for(int i=a;i<b;i++)
+#define bfor(i,a,b) for(int i=a;i>=b;i--)
+#define PI 3.14159265358979323846
+#define SP(x) setprecision(x)
+#define reflex ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define M 1000007
+
+void solve(){
+    ll n;
+    cin>>n;
+    ll ans=((n*(3*n+1))/2)%M;
+    cout<<ans<<endl;
+}
+int main(){
+    reflex;
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }    
+}
+
