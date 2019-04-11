becd20ce08a5459323f59990a8f5ec3a13873620
the-sam-mit <47500983+the-sam-mit@users.noreply.github.com>
2019-04-11 08:21:19 +0530

    SPOJ - - Basic Maths

---
+++ MAXLN - THE MAX LINES.cpp
@@ -0,0 +1,29 @@
+//Basic Maths
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ull unsigned long long int
+#define ff first 
+#define ss second
+#define pb(x) push_back(x)
+#define mp(x,y) make_pair(x,y)
+#define in(x) insert(x)
+#define rep(i,a,b)  for (__typeof((b)) i=(a);i<=(b);i++)
+#define nrep(i,a,b)  for (__typeof((b)) i=(b);i>=(a);i--)
+#define ffor(i,a,b) for(int i=a;i<b;i++)
+#define bfor(i,a,b) for(int i=a;i>=b;i--)
+#define PI 3.14159265358979323846
+#define SP(x) setprecision(x)
+#define reflex ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+int main(){
+    reflex;
+    int t;
+    cin>>t;
+    rep(i,1,t){
+        double r;
+        cin>>r;
+        double ans = (4*r*r)+.25;
+        cout<<"Case "<<i<<": "<<fixed<<SP(5)<<ans<<endl;
+    }
+}
